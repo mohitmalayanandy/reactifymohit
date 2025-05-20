48aca5844786929367ff4e8c12521cdcff0c5a1e
Mohit Malaya Nandy <mohitmalayanandy@gmail.com>
2025-05-20 23:34:39 +0530

    null

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
-import { resolve } from 'path';
-import fs from 'fs';
+
 
 export default defineConfig({
   base: '/reactifymohit/',
   plugins: [
     react(),
     tailwindcss(),
-    {
-      name: 'copy-index-to-404',
-      closeBundle() {
-        const indexPath = resolve(__dirname, 'dist/index.html');
-        const notFoundPath = resolve(__dirname, 'dist/404.html');
-        fs.copyFileSync(indexPath, notFoundPath);
-      }
-    }
   ]
 });
